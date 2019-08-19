ff072f50bba31b47a6f81069c3aa0df3ce0429fb
Igor Mocherad <mocherad86@gmail.com>
2019-08-19 21:27:42 +0300

    implement webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+let path = require('path');
+
+let conf = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+        publicPath: 'dist/'
+    },
+    // remove for build
+    devServer: {
+        overlay: true
+    }
+}
+
+module.exports = conf;
